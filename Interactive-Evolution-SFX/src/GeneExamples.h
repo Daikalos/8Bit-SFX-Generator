8579b2203573db545f4c0b80e9b0d70851670ac9
Tobias G <31646748+Daikalos@users.noreply.github.com>
2022-03-03 19:41:46 +0100

    small

--- Interactive-Evolution-SFX/src/GeneExamples.h
+++ Interactive-Evolution-SFX/src/GeneExamples.h
@@ -4,10 +4,10 @@
 
 static const std::string examples[16] =
 {
-	R"(poke 1 130
+	R"(poke 24 14
+poke 1 130
 poke 5 9
 poke 15 30
-poke 24 14
 poke 4 21
 sample 1000
 poke 4 20
@@ -61,7 +61,64 @@ poke 15 2
 poke 6 7
 sample 851)",
 
-	R"()",
+	R"(poke 24 14
+poke 10 45
+poke 3 94
+poke 7 17
+poke 12 78
+poke 4 88
+poke 15 27
+poke 8 78
+poke 13 49
+poke 16 91
+poke 0 56
+poke 17 19
+poke 9 87
+poke 1 16
+poke 6 98
+poke 18 35
+poke 21 42
+poke 11 17
+poke 2 13
+sample 250
+poke 15 54
+poke 2 44
+poke 20 33
+poke 5 25
+poke 4 8
+poke 9 31
+poke 1 16
+poke 8 44
+poke 10 87
+poke 12 81
+poke 6 57
+poke 13 93
+poke 19 44
+poke 22 63
+poke 17 30
+poke 14 89
+poke 23 77
+poke 0 33
+sample 730
+poke 12 82
+poke 21 49
+poke 2 23
+poke 19 66
+poke 15 97
+poke 13 36
+poke 10 85
+poke 20 71
+poke 11 95
+poke 22 53
+poke 14 62
+poke 4 67
+poke 8 34
+poke 7 74
+poke 3 53
+poke 23 36
+poke 17 39
+poke 1 13
+sample 153)",
 
 	R"()",
 
